17d4138d9ba0bfbf23d49cbe50e228d4162f7f7c
takwa <blgeyatatou@gmail.com>
2022-01-07 14:31:36 +0100

    fix

--- routes.js
+++ routes.js
@@ -15,26 +15,25 @@ app.post('/infos/headers', (req, res) => {
  res.send(req.headers);
 });
 
-app.post('/payload', (req, res) => {
- const payload = req.body;
-
- const date = new Date(payload.birthdate);
+app.post('/payload', async function (req, res) {
+  
+ const date = new Date(req.body.birthdate);
  const month = Date.now() - date.getTime();
- const age_diff = new Date(month);
- const year = age_diff.getUTCFullYear();
+ const diff = new Date(month);
+ const year = diff.getUTCFullYear();
  const age = Math.abs(year - 1970);
 
  if (age > 18) {
-     res.status(200).json({ message: `Welcome ${payload.firstname}` });
+     res.status(200).json({ message: `Welcome ${req.body.firstname}` });
  };
    res.status(403).json({ message: "Forbidden" });
 });
 
-app.get('/rick-roll', (req, res) => {
+app.get('/rick-roll', async function (req, res) {
  res.status(200).redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 });
 
-app.delete('/custom-header', (req, res) => {
+app.delete('/custom-header', async function (req, res) {
  res.set({ message: "Hello world !" }).end();
 });
 
